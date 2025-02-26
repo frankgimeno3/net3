d3d75582daac5e57504b2ce44ea0d1408a92ec47
frank gimeno <frankgimeno3@gmail.com>
2025-02-26 18:10:58 +0100

    navbar se fija arriba

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,17 +13,17 @@ export default function Home() {
   const [lang, setLang] = useState<"ESP" | "ENG">("ESP");
 
   return (
-    <div className="flex flex-col min-h-screen justify-between text-white bg-gradient-to-b from-black to-slate-900 animate-gradient">
-      <MainNav section={section} setSection={setSection} lang={lang} setLang={setLang} />
-
-      <div className="flex flex-col">
+    <div className="relative flex flex-col min-h-screen justify-between text-white bg-gradient-to-b from-black to-slate-900 animate-gradient">
+      <div className="fixed top-0 left-0 w-full bg-black bg-opacity-80 z-50">
+        <MainNav section={section} setSection={setSection} lang={lang} setLang={setLang} />
+      </div>
+      <div className="flex flex-col mt-20 pt-16">
         <QuienSomos lang={lang} />
         <QueHacemos lang={lang} />
         <PlanContenidos lang={lang} />
         <CincoPasos lang={lang} />
         <Contáctanos lang={lang} />
       </div>
-      
       <footer className="p-5 w-full bg-white bg-opacity-5">
         <FooterSection lang={lang} />
       </footer>
