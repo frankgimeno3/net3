b8ef393bea8242777c8b690bcb6cb09b85a942ed
frank gimeno <frankgimeno3@gmail.com>
2025-09-20 18:33:41 +0200

    creo page admin

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+
+interface AdminProps {
+  
+}
+
+const Admin: FC<AdminProps> = ({ }) => {
+  return (
+    <div>
+        <p>Aquí va un formulario de admin</p>
+    </div>
+  );
+};
+
+export default Admin;
