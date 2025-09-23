10bdaf6509a2a63e0bb831cc4355fd3200943ad8
frank gimeno <frankgimeno3@gmail.com>
2025-09-23 22:49:14 +0200

    reestructuro footer con nuevo contenido

--- src/app/components/navs/FooterSection.tsx
+++ src/app/components/navs/FooterSection.tsx
@@ -2,7 +2,7 @@
 
 import React, { FC } from 'react';
 import footerContent from '../content/FooterContent.json';
-import { useRouter } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
 import Image from 'next/image';
 import { useLanguage } from '@/app/context/LanguageContext';
 
@@ -16,6 +16,7 @@ const FooterSection: FC<FooterSectionProps> = ({ id }) => {
   const { lang } = useLanguage();
   const content = footerContent[lang] || footerContent["ENG"];
   const router = useRouter();
+  const pathname = usePathname();
 
   const handleRedirection = (redirection: string) => {
     router.push(redirection);
@@ -24,12 +25,11 @@ const FooterSection: FC<FooterSectionProps> = ({ id }) => {
   return (
     <footer
       id={id}
-      className="flex flex-col md:flex-row justify-between p-5 bg-black text-white px-4 sm:px-24 py-12 z-50 
-      w-full relative items-center"
+      className="flex flex-col md:flex-row justify-between p-5 bg-black text-white px-4 sm:px-24 py-12 z-50 w-full relative items-center"
       style={{ backgroundColor: "rgb(28, 28, 28)" }}
     >
+      {/* Logo, contacto y copyrights */}
       <div className="flex flex-col text-center sm:text-left sm:px-12 mt-12 gap-4 md:gap-0">
-
         <div
           className="flex flex-row text-xl md:text-4xl cursor-pointer"
           onClick={() => handleRedirection('/')}
@@ -38,7 +38,6 @@ const FooterSection: FC<FooterSectionProps> = ({ id }) => {
           <div className="hidden sm:block pb-5">
             <Image src="/net3logoesBlanco.png" alt="net3" height={180} width={180} />
           </div>
-
           <div className="block sm:hidden pb-5">
             <Image src="/net3Complete.svg" alt="net3" height={180} width={180} />
           </div>
@@ -61,25 +60,25 @@ const FooterSection: FC<FooterSectionProps> = ({ id }) => {
         </div>
       </div>
 
+      {/* Secciones: Servicios y Links */}
       <div className="flex flex-col sm:flex-row text-sm mt-12 sm:mt-6 text-center sm:text-left md:pr-36 ">
         {(Object.keys(content.sections) as SectionKey[]).map((section) => (
           <div key={section} className="flex flex-col w-full sm:px-12 py-6">
             <p className="font-bold text-white text-lg">{content.sections[section]}</p>
             <div className="flex flex-col md:py-3">
-              {content[section as SectionKey].map((item: string, index: number) => (
-                <p
-                  key={index}
-                  className="cursor-pointer hover:text-white text-gray-500 py-1"
-                  onClick={() => {
-                    const redirection = content.redirections?.[section]?.[index];
-                    if (redirection) {
-                      handleRedirection(redirection);
-                    }
-                  }}
-                >
-                  {item}
-                </p>
-              ))}
+              {content[section].map((item: string, index: number) => {
+                const redirection = content.redirections?.[section]?.[index];
+                const isActive = redirection === pathname; // detecta si estamos en esa página
+                return (
+                  <p
+                    key={index}
+                    className={`cursor-pointer py-1 ${isActive ? 'text-white font-bold' : 'text-gray-500 hover:text-white'}`}
+                    onClick={() => redirection && handleRedirection(redirection)}
+                  >
+                    {item}
+                  </p>
+                );
+              })}
             </div>
           </div>
         ))}
