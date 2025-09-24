4f827edc27259b2bc44a9d0557b14f595ece227b
frank gimeno <frankgimeno3@gmail.com>
2025-09-24 16:58:43 +0200

    agrego iconitos a servicios

--- src/app/components/sections/aboutus/2WhatWeDo/QueHacemos.tsx
+++ src/app/components/sections/aboutus/2WhatWeDo/QueHacemos.tsx
@@ -5,6 +5,18 @@ import { useRouter } from "next/navigation";
 import ActionButton from "../../ActionButton";
 import Image from "next/image";
 
+// Importamos los SVGs
+import PR from "./svg/PR";
+import Video from "./svg/Video";
+ import Banners from "./svg/Banners";
+import Diseno from "./svg/Diseno";
+import RRSS from "./svg/RRSS";
+import Mailing from "./svg/Mailing";
+import Funnel from "./svg/Funnel";
+import Money from "./svg/Money";
+import Up from "./svg/Up";
+import Magazine from "./svg/Magazine";
+
 const QueHacemos: FC = () => {
   const { lang } = useLanguage();
   const content = content2[lang];
@@ -23,19 +35,35 @@ const QueHacemos: FC = () => {
   const sectionData = [
     {
       title: content.Contenidos.titular,
-      resumen: lang === "ESP" ? (content.Contenidos as any).resumen : (content.Contenidos as any).summary,
+      resumen:
+        lang === "ESP"
+          ? (content.Contenidos as any).resumen
+          : (content.Contenidos as any).summary,
       path: "/pages/details/contents",
     },
     {
       title: content.Publicidad.titular,
-      resumen: lang === "ESP" ? (content.Publicidad as any).resumen : (content.Publicidad as any).summary,
+      resumen:
+        lang === "ESP"
+          ? (content.Publicidad as any).resumen
+          : (content.Publicidad as any).summary,
       path: "/pages/details/adv",
     },
     {
       title: content.Funnel.titular,
-      resumen: lang === "ESP" ? (content.Funnel as any).resumen : (content.Funnel as any).summary,
+      resumen:
+        lang === "ESP"
+          ? (content.Funnel as any).resumen
+          : (content.Funnel as any).summary,
       path: "/pages/details/funnel",
     },
+  ];
+
+  // Arrays de íconos por fila
+  const iconsByRow = [
+    [PR, Video, RRSS], // primera iteración
+    [Banners, Magazine, Up], // segunda
+    [Mailing, Funnel, Money], // tercera
   ];
 
   return (
@@ -55,8 +83,14 @@ const QueHacemos: FC = () => {
           {lang === "ESP" ? "¿Qué hacemos?" : "What we do"}
         </h1>
         {sectionData.map((section, i) => (
-          <div key={i} className="my-5 flex flex-row w-full justify-between items-center bg-white shadow rounded-xl pr-3">
-            <div className="hidden md:block relative shadow-xl   rounded-xl " style={{ width: "450px", height: "450px" }}>
+          <div
+            key={i}
+            className="my-5 flex flex-row w-full justify-between items-center bg-white shadow rounded-xl pr-3"
+          >
+            <div
+              className="hidden md:block relative shadow-xl rounded-xl"
+              style={{ width: "450px", height: "450px" }}
+            >
               <Image
                 src={imagePaths[i]}
                 fill
@@ -66,19 +100,36 @@ const QueHacemos: FC = () => {
               />
             </div>
             <div className="flex flex-col w-1/2 items-top">
-              <h2 className="text-left font-bold mb-2 md:mb-8 md:max-w-lg">{section.title}</h2>
+              <h2 className="text-left font-bold mb-2 md:mb-8 md:max-w-lg">
+                {section.title}
+              </h2>
               <div className="flex flex-row items-center justify-between">
                 <div className="flex flex-col md:max-w-xl mr-12 md:gap-3">
-                  <p className="leading-relaxed text-md md:text-lg">{section.resumen}</p>
+                  <p className="leading-relaxed text-md md:text-lg">
+                    {section.resumen}
+                  </p>
+                  <div className="flex flex-row gap-4 flex-wrap">
+                    {iconsByRow[i].map((Icon, idx) => (
+                      <div
+                        key={idx}
+                        className="flex bg-gray-400 w-16 h-16 items-center justify-center rounded-lg shadow mt-4"
+                      >
+                        <Icon />
+                      </div>
+                    ))}
+                  </div>
                   <ActionButton
-                    label={lang === "ESP" ? "Quieres saber más?" : "Want to know more?"}
+                    label={
+                      lang === "ESP"
+                        ? "Quieres saber más?"
+                        : "Want to know more?"
+                    }
                     onClick={() => router.push(section.path)}
                     align="left"
                   />
                 </div>
               </div>
             </div>
-
           </div>
         ))}
       </div>
